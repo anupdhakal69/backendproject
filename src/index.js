a9e683a80e144caa69ea7d2c7d518be269eac6ce
Anup D <dhakalanup96@gmail.com>
2024-08-30 19:53:47 +0545

    some files added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import dotenv from "dotenv"
+import "dotenv/config";
 import connectDB from "./db/index.js";
-
-dotenv.config({
-    path: "./env"
-})
+import { app } from "./app.js";
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server running on port ${process.env.PORT || 3000}`)
+    })
+})
+.catch(error => {
+    console.error("Error connecting to database", error);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+
+export const asyncHandler = (func) => async (req, res, next) =>{
+     try {
+        await func(req, res, next);
+     } catch (error) {
+        res.status(error.code || 500).json({message: error.message || "An error occurred"});
+     }
+}
